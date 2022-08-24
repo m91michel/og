e1355a59689e00e3d859cec064345fe40732f8f7
Mathias Michel <m.michel@klarx.com>
2022-08-24 08:10:16 +0200

    feat: adjust blog template

--- src/pages/api/blog.ts
+++ src/pages/api/blog.ts
@@ -46,7 +46,7 @@ export default withOGImage<'query', keyof typeof QueryEnum>({
               <div class="split">
                 <div class="left">
                   <h3 class="link">
-                    theodorusclarence.com/blog
+                    mathias.rocks/blog
                   </h3>
                   <h1 class="title">
                     <span class="gradient">
@@ -56,8 +56,8 @@ export default withOGImage<'query', keyof typeof QueryEnum>({
                   <div class="social">
                     <img class="social_img" src="https://res.cloudinary.com/theodorusclarence/image/upload/c_fill,g_auto:face,h_1000,w_1000/v1636335597/theodorusclarence/clarence_gu3cxx.jpg">
                     <div class="social_info">
-                      <p class="name">Theodorus Clarence</p>
-                      <p class="twitter">@th_clarence</p>
+                      <p class="name">Mathias Michel</p>
+                      <p class="twitter">@m91michel</p>
                     </div>
                   </div>
                 </div>
